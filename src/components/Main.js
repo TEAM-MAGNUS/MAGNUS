6b61c71d406830508770622798a8eab73deb50cc
changjini32 <84197474+changjini32@users.noreply.github.com>
2022-10-06 00:57:52 +0900

    header animation까지 추가

---
+++ src/components/Main.js
@@ -0,0 +1,53 @@
+import React from "react";
+import ReactFullpage from "@fullpage/react-fullpage";
+
+import { IoIosArrowUp, IoIosArrowDown } from "react-icons/io";
+
+import logo from "../asset/main/logo.png";
+
+function Main() {
+  return (
+    <>
+      <ReactFullpage
+        scrollOverflow={true}
+        render={({ fullpageApi }) => (
+          <div id="fullpage-wrapper">
+            <div className="section">
+              <div className="div-main-section-01">
+                <img className="img-main" src={logo} alt="" />
+              </div>
+              <IoIosArrowDown
+                className="icon-main-arrow-down"
+                size="20"
+                onClick={() => fullpageApi.moveSectionDown()}
+              />
+            </div>
+            <div className="section">
+              <IoIosArrowUp
+                className="icon-main-arrow-up"
+                size="20"
+                onClick={() => fullpageApi.moveSectionUp()}
+              />
+              <div className="div-main-section-01">홍보페이지1</div>
+              <IoIosArrowDown
+                className="icon-main-arrow-down"
+                size="20"
+                onClick={() => fullpageApi.moveSectionDown()}
+              />
+            </div>
+            <div className="section">
+              <IoIosArrowUp
+                className="icon-main-arrow-up"
+                size="20"
+                onClick={() => fullpageApi.moveSectionUp()}
+              />
+              <div className="div-main-section-01">홍보페이지2</div>
+            </div>
+          </div>
+        )}
+      />
+    </>
+  );
+}
+
+export default Main;
