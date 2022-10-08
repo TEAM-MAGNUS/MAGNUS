ef29b48098c99f4313bfaf2f145a6bea71141c39
GyuYong Jeong <84078044+Gyudol1231@users.noreply.github.com>
2022-10-08 13:27:20 +0900

    Update Attendance.js (#1)

--- src/components/Attendance.js
+++ src/components/Attendance.js
@@ -1,6 +1,29 @@
 import React from "react";
 import profile from "../asset/attendance/profile.png";
 
+function write(ip, date) {
+  console.log(`ip: ${ip === "210.94.182.243" ? "true" : "false"}`);
+  console.log(`날짜: ${date.getMonth()}월 ${date.getDate()}일`);
+  const post = {
+    query:
+      "INSERT INTO magnus_attendance (ip, date) VALUES ('" +
+      ip + "', '" +
+
+      date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +
+      date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() +
+      "');"
+  };
+
+  console.log(post.query);
+  fetch("https://hansori.net:443/SQL1", {
+    method: "post",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(post),
+  }).then(() => {
+    console.log("success");
+  });
+}
+
 function Attendance() {
   var today = new Date();
   return (
@@ -11,7 +34,17 @@ function Attendance() {
       </div>
       <div className="div-attendance-date">{today.toLocaleDateString()}</div>
       {today.getDay() === 0 || today.getDay() === 5 || today.getDay() === 6 ? (
-        <div className="div-attendance-check">출석</div>
+        <div 
+          className="div-attendance-check"
+          onClick={async (e) => {
+            const Location = await fetch("https://geolocation-db.com/json/")
+            const location = await Location.json();
+            today = new Date();
+            location.IPv4 === "210.94.182.243" ? write(location.IPv4, today) : console.log("ip가 다릅니다.");
+          }}
+        >
+          출석
+        </div>
       ) : (
         <div className="div-attendance-check">ㅡ</div>
       )}
