d27ab18826bc3304717da8e189bfcf6ffa42fbf0
GGyong <jgy123117@naver.com>
2022-10-15 02:54:37 +0900

    Squashed commit of the following:
    commit 10d2cea2223578a50529f9a80809ee14d4ef659f
    Author: changjini32 <84197474+changjini32@users.noreply.github.com>
    Date:   Thu Oct 13 00:25:55 2022 +0900
    
        Update Attendance.js
    
    commit b1267e441fbef72d00ed371274b5b154981bb7b4
    Author: changjini32 <84197474+changjini32@users.noreply.github.com>
    Date:   Thu Oct 13 00:19:35 2022 +0900
    
        .
    
    commit 24648dd98752e9c531f3980b3ec20ece23f41df8
    Author: changjini32 <84197474+changjini32@users.noreply.github.com>
    Date:   Wed Oct 12 15:22:50 2022 +0900
    
        .
    
    commit 9073e3a687daa80ffec6d52888a0116d4e86b939
    Author: changjini32 <84197474+changjini32@users.noreply.github.com>
    Date:   Wed Oct 12 14:57:27 2022 +0900
    
        .
    
    commit 15fe8db0cb69940e922aab497ebe646dc3cc766f
    Author: changjini32 <84197474+changjini32@users.noreply.github.com>
    Date:   Wed Oct 12 14:52:17 2022 +0900
    
        .
    
    commit 810fd7bdcb0b01ce69cd79ed8d37edfc21e7411c
    Author: changjini32 <84197474+changjini32@users.noreply.github.com>
    Date:   Wed Oct 12 14:51:49 2022 +0900
    
        .

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+const cors = require("cors");
 const port = 8080;
 const mysql = require("mysql");
 
-const connection = mysql.connection({
+const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "0000",
-  database: "hansori",
+  database: "magnus",
 });
 
 connection.connect();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
 app.post("/SQL1", (req, res) => {
   const post = req.body.query;
-
   connection.query(post, function (err, rows, fields) {
     if (err) {
       console.log("전송 실패");
@@ -27,13 +32,11 @@ app.post("/SQL1", (req, res) => {
 });
 app.post("/SQL2", (req, res) => {
   const post = req.body.query;
-
   connection.query(post, function (err, rows, fields) {
     if (err) {
       console.log("전송 실패");
     } else {
       console.log("전송 성공");
-      //console.log(rows[0]);
       res.send(rows);
     }
   });
---
+++ src/components/Calendar.js
@@ -0,0 +1,314 @@
+import React, { useState } from "react";
+import dayjs from "dayjs";
+import {
+  AiOutlinePlus,
+  AiOutlineClose,
+  AiOutlineMinus,
+  AiOutlineCheck,
+} from "react-icons/ai";
+
+const today = dayjs(new Date());
+const td = new Date();
+
+function Calendar() {
+  const year = td.getFullYear();
+  const month = td.getMonth();
+  const date = td.getDate();
+
+  var calendar = [
+    [
+      { date: null, schedule: "a" },
+      { date: null, schedule: "b" },
+      { date: null, schedule: "v" },
+    ],
+    [
+      { date: null, schedule: "a" },
+      { date: null, schedule: "b" },
+      { date: null, schedule: "c" },
+    ],
+    [
+      { date: null, schedule: "d" },
+      { date: null, schedule: "e" },
+      { date: null, schedule: "f" },
+    ],
+    [
+      { date: null, schedule: "g" },
+      { date: null, schedule: "h" },
+      { date: null, schedule: "i" },
+    ],
+    [
+      { date: null, schedule: "j" },
+      { date: null, schedule: "k" },
+      { date: null, schedule: "l" },
+    ],
+  ];
+
+  const setCalendar = () => {
+    var tmp;
+    var first = [{ date: 0, day: 0 }];
+    for (let i = 1; i <= 7; ++i) {
+      tmp = new Date(year, month, i).getDay();
+      if (tmp === 0 || tmp === 5 || tmp === 6) {
+        first = { date: i, day: tmp };
+        break;
+      }
+    }
+
+    switch (first.day) {
+      case 5: {
+        calendar[0][0].date = first.date;
+        calendar[0][1].date = first.date + 1;
+        calendar[0][2].date = first.date + 2;
+        first.date += 7;
+        break;
+      }
+      case 6: {
+        calendar[0][1].date = first.date;
+        calendar[0][2].date = first.date + 1;
+        first.date += 6;
+        break;
+      }
+      case 0: {
+        calendar[0][2].date = first.date;
+        first.date += 5;
+        break;
+      }
+      default:
+    }
+
+    var week = 1;
+    while (week < 5) {
+      calendar[week][0].date = first.date;
+      calendar[week][1].date = first.date + 1;
+      calendar[week][2].date = first.date + 2;
+      first.date += 7;
+      week += 1;
+    }
+  };
+  setCalendar();
+
+  const [isOpen1, setOpen1] = useState(false);
+  const [isOpen2, setOpen2] = useState(false);
+  const [now, setNow] = useState({ week: 0, date: 0, day: 0 });
+  const [schedule, setSchedule] = useState("");
+
+  const openSchedule = (week, date, day) => {
+    setNow({ week: week, date: date, day: day });
+    getSchedule(date);
+  };
+
+  const [text, setText] = useState("");
+
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const getSchedule = (date) => {
+    const post = {
+      query:
+        "SELECT schedule from magnus_schedule WHERE (schedule_date = '" +
+        year +
+        "-" +
+        (month + 1) +
+        "-" +
+        date +
+        "');",
+    };
+    fetch("http://localhost:8080/SQL1", {
+      method: "post",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(post),
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        setSchedule(json.schedule);
+      })
+      .catch((e) => {
+        setSchedule("등록된 일정이 없습니다.");
+      })
+      .then(() => {
+        setOpen1(true);
+      });
+  };
+
+  const writeSchedule = (
+    <>
+      <div className="div-calendar-schedule-write">
+        <input
+          className="input-calendar-schedule-write"
+          maxLength={30}
+          onChange={onChange}
+          value={text}
+        ></input>
+      </div>
+      {text === "" ? (
+        <></>
+      ) : (
+        <AiOutlineCheck
+          className="button-calendar-schedule-add"
+          onClick={() => addSchedule(now.date, text)}
+        />
+      )}
+    </>
+  );
+
+  const addSchedule = (date, schedule) => {
+    const post = {
+      query:
+        "INSERT INTO magnus_schedule VALUES ('" +
+        year +
+        "-" +
+        (month + 1) +
+        "-" +
+        date +
+        "', '" +
+        schedule +
+        "');",
+    };
+    console.log(post.query);
+    fetch("http://localhost:8080/SQL1", {
+      method: "post",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(post),
+    }).then(window.location.reload());
+  };
+
+  const removeSchedule = (date) => {
+    const post = {
+      query:
+        "DELETE FROM magnus_schedule WHERE ( schedule_date = '" +
+        year +
+        "-" +
+        (month + 1) +
+        "-" +
+        date +
+        "');",
+    };
+    console.log(post.query);
+    fetch("http://localhost:8080/SQL1", {
+      method: "post",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(post),
+    }).then(window.location.reload());
+  };
+
+  const showSchedule = (
+    <>
+      <div className="div-calendar-schedule-section">
+        <div className="div-calendar-schedule-01">
+          {year + "." + (month + 1) + "." + now.date}
+        </div>
+        <div className="div-calendar-schedule-02">{schedule}</div>
+      </div>
+      {isOpen2 ? writeSchedule : <></>}
+      {schedule === "등록된 일정이 없습니다." ? (
+        isOpen2 ? (
+          <AiOutlineClose
+            className="button-calendar-schedule-write"
+            onClick={() => {
+              setOpen2(false);
+              setText("");
+            }}
+          />
+        ) : (
+          <AiOutlinePlus
+            className="button-calendar-schedule-write"
+            onClick={() => {
+              setOpen2(true);
+            }}
+          />
+        )
+      ) : (
+        <AiOutlineMinus
+          className="button-calendar-schedule-write"
+          onClick={() => {
+            if (window.confirm("정말 삭제하시겠습니까?")) {
+              removeSchedule(now.date);
+            }
+          }}
+        />
+      )}
+    </>
+  );
+
+  const [clicked, setClicked] = useState({ week: null, date: null });
+
+  const showCalendar = (
+    <table className="div-calendar">
+      <thead>
+        <tr>
+          <th>FRI</th>
+          <th>SAT</th>
+          <th>SUN</th>
+        </tr>
+      </thead>
+      <tbody>
+        {calendar.map((w, index) => (
+          <tr>
+            {w[0].date < date ? (
+              <th className="calendar-p">{w[0].date}</th>
+            ) : (
+              <th
+                className={
+                  clicked.week === index && clicked.date === w[0].date
+                    ? "calendar-t"
+                    : "calendar-f"
+                }
+                onClick={() => {
+                  openSchedule(index, w[0].date, 0);
+                  setClicked({ week: index, date: w[0].date });
+                }}
+              >
+                {w[0].date}
+              </th>
+            )}
+            {w[1].date < date ? (
+              <th className="calendar-p">{w[1].date}</th>
+            ) : (
+              <th
+                className={
+                  clicked.week === index && clicked.date === w[1].date
+                    ? "calendar-t"
+                    : "calendar-f"
+                }
+                onClick={() => {
+                  openSchedule(index, w[1].date, 1);
+                  setClicked({ week: index, date: w[1].date });
+                }}
+              >
+                {w[1].date}
+              </th>
+            )}
+            {w[2].date < date ? (
+              <th className="calendar-p">{w[2].date}</th>
+            ) : (
+              <th
+                className={
+                  clicked.week === index && clicked.date === w[2].date
+                    ? "calendar-t"
+                    : "calendar-f"
+                }
+                onClick={() => {
+                  openSchedule(index, w[2].date, 2);
+                  setClicked({ week: index, date: w[2].date });
+                }}
+              >
+                {w[2].date}
+              </th>
+            )}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+
+  return (
+    <div className="div-calendar-section">
+      <div className="div-month">{today.format("YYYY.MM")}</div>
+      <div className="div-attendance-section-01">{showCalendar}</div>
+      {isOpen1 ? showSchedule : <></>}
+    </div>
+  );
+}
+
+export default Calendar;
