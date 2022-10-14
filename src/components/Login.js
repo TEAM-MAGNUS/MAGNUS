77b4673ac1f5b226355d0b0f89e80aa199e2572e
GGyong <jgy123117@naver.com>
2022-10-15 07:30:37 +0900

    1015z
    - 렌더
    - 최초 마운트 useEffect()
    - promise 공부하기

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,6 @@
 import React from "react";
 import loginbtn from "../asset/login/kakao_login.png";
-
-export const REST_API_KEY = '08066204288ab5d47ffcf2d53a17bdad';
-export const REDIRECT_URI = 'http://localhost:3000/kakaologin';
+import { REST_API_KEY, REDIRECT_URI } from "./LoginData";
 
 function Login() {
     const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
