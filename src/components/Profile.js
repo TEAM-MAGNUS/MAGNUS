319d68d0d5ac201d56deb8ace0de853cfb4e054d
GGyong <jgy123117@naver.com>
2022-10-15 02:02:31 +0900

    Squashed commit of the following:
    commit 10d2cea2223578a50529f9a80809ee14d4ef659f
    Author: changjini32 <84197474+changjini32@users.noreply.github.com>
    Date:   Thu Oct 13 00:25:55 2022 +0900
    
        Update Attendance.js
    
    commit b1267e441fbef72d00ed371274b5b154981bb7b4
    Author: changjini32 <84197474+changjini32@users.noreply.github.com>
    Date:   Thu Oct 13 00:19:35 2022 +0900
    
        .
    
    commit 24648dd98752e9c531f3980b3ec20ece23f41df8
    Author: changjini32 <84197474+changjini32@users.noreply.github.com>
    Date:   Wed Oct 12 15:22:50 2022 +0900
    
        .
    
    commit 9073e3a687daa80ffec6d52888a0116d4e86b939
    Author: changjini32 <84197474+changjini32@users.noreply.github.com>
    Date:   Wed Oct 12 14:57:27 2022 +0900
    
        .
    
    commit 15fe8db0cb69940e922aab497ebe646dc3cc766f
    Author: changjini32 <84197474+changjini32@users.noreply.github.com>
    Date:   Wed Oct 12 14:52:17 2022 +0900
    
        .
    
    commit 810fd7bdcb0b01ce69cd79ed8d37edfc21e7411c
    Author: changjini32 <84197474+changjini32@users.noreply.github.com>
    Date:   Wed Oct 12 14:51:49 2022 +0900
    
        .

---
+++ src/components/Profile.js
@@ -0,0 +1,67 @@
+import React from "react";
+import profile from "../asset/profile/profile.png";
+import dayjs from "dayjs";
+
+function write(ip, date) {
+  console.log(`ip: ${ip === "210.94.182.243" ? "true" : "false"}`);
+  console.log(`날짜: ${date.getMonth()}월 ${date.getDate()}일`);
+  const post = {
+    query:
+      "INSERT INTO magnus_attendance (ip, date) VALUES ('" +
+      ip +
+      "', '" +
+      date.getFullYear() +
+      "-" +
+      (date.getMonth() + 1) +
+      "-" +
+      date.getDate() +
+      " " +
+      date.getHours() +
+      ":" +
+      date.getMinutes() +
+      ":" +
+      date.getSeconds() +
+      "');",
+  };
+
+  console.log(post.query);
+  fetch("https://hansori.net:443/SQL1", {
+    method: "post",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(post),
+  }).then(() => {
+    console.log("success");
+  });
+}
+
+function Profile() {
+  const today = dayjs(new Date());
+  const td = new Date();
+  return (
+    <>
+      <div className="div-profile-profile">
+        <img className="img-profile-profile" src={profile} alt="" />
+        name1
+      </div>
+      <div className="div-profile-date">{today.format("YYYY.MM.DD")}</div>
+      {td.getDay() === 0 || td.getDay() === 5 || td.getDay() === 6 ? (
+        <div
+          className="div-profile-check"
+          onClick={async (e) => {
+            const Location = await fetch("https://geolocation-db.com/json/");
+            const location = await Location.json();
+            location.IPv4 === "210.94.182.243"
+              ? write(location.IPv4, td)
+              : console.log("ip가 다릅니다.");
+          }}
+        >
+          출석
+        </div>
+      ) : (
+        <div className="div-profile-check">ㅡ</div>
+      )}
+    </>
+  );
+}
+
+export default Profile;
