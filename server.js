f717b9489948a7b260de796d4c5c1170f92dee31
GGyong <jgy123117@naver.com>
2022-10-15 02:13:39 +0900

    Revert "Squashed commit of the following:"
    This reverts commit 319d68d0d5ac201d56deb8ace0de853cfb4e054d.

--- server.js
+++ server.js
@@ -1,25 +1,20 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
-const cors = require("cors");
 const port = 8080;
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
+const connection = mysql.connection({
   host: "localhost",
   user: "root",
   password: "0000",
-  database: "magnus",
+  database: "hansori",
 });
 
 connection.connect();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cors());
-
 app.post("/SQL1", (req, res) => {
   const post = req.body.query;
+
   connection.query(post, function (err, rows, fields) {
     if (err) {
       console.log("전송 실패");
@@ -32,11 +27,13 @@ app.post("/SQL1", (req, res) => {
 });
 app.post("/SQL2", (req, res) => {
   const post = req.body.query;
+
   connection.query(post, function (err, rows, fields) {
     if (err) {
       console.log("전송 실패");
     } else {
       console.log("전송 성공");
+      //console.log(rows[0]);
       res.send(rows);
     }
   });
