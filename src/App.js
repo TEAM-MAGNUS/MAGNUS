4d71ba7d87cb78b6e027a8be4bcf293b06a18a00
GGyong <jgy123117@naver.com>
2022-10-15 02:54:37 +0900

    Squashed commit of the following:
    commit 10d2cea2223578a50529f9a80809ee14d4ef659f
    Author: changjini32 <84197474+changjini32@users.noreply.github.com>
    Date:   Thu Oct 13 00:25:55 2022 +0900
    
        Update Attendance.js
    
    commit b1267e441fbef72d00ed371274b5b154981bb7b4
    Author: changjini32 <84197474+changjini32@users.noreply.github.com>
    Date:   Thu Oct 13 00:19:35 2022 +0900
    
        .
    
    commit 24648dd98752e9c531f3980b3ec20ece23f41df8
    Author: changjini32 <84197474+changjini32@users.noreply.github.com>
    Date:   Wed Oct 12 15:22:50 2022 +0900
    
        .
    
    commit 9073e3a687daa80ffec6d52888a0116d4e86b939
    Author: changjini32 <84197474+changjini32@users.noreply.github.com>
    Date:   Wed Oct 12 14:57:27 2022 +0900
    
        .
    
    commit 15fe8db0cb69940e922aab497ebe646dc3cc766f
    Author: changjini32 <84197474+changjini32@users.noreply.github.com>
    Date:   Wed Oct 12 14:52:17 2022 +0900
    
        .
    
    commit 810fd7bdcb0b01ce69cd79ed8d37edfc21e7411c
    Author: changjini32 <84197474+changjini32@users.noreply.github.com>
    Date:   Wed Oct 12 14:51:49 2022 +0900
    
        .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Attendance from "./components/Attendance";
+import Calendar from "./components/Calendar";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Login from "./components/Login";
 import KakaoLogin from "./components/KakaoLogin";
+import Profile from "./components/Profile";
+import Ranking from "./components/Ranking";
 
 function App() {
   return (
@@ -12,9 +15,12 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" exact={true} element={<Main />} />
+          <Route path="/profile" element={<Profile />} />
           <Route path="/attendance" element={<Attendance />} />
           <Route path="/login" element={<Login />} />
           <Route path="/kakaoLogin" element={<KakaoLogin />} />
+          <Route path="/ranking" element={<Ranking />} />
+          <Route path="/calendar" element={<Calendar />} />
         </Routes>
       </BrowserRouter>
     </>
