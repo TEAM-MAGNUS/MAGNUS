7e5c789911c3209e943993040f3a9d67458aedac
changjini32 <84197474+changjini32@users.noreply.github.com>
2022-10-19 05:02:38 +0900

    Update Member.js

--- src/components/Member.js
+++ src/components/Member.js
@@ -53,60 +53,61 @@ function Member() {
 
   return (
     <>
-      {isOpen && <MemberAttendance name={name} />}
-      <div className="div-ranking">
-        <div className="div-notice-header"></div>
-        {isOpen ? (
+      {isOpen ? (
+        <>
+          <MemberAttendance name={name} />
           <HiOutlineArrowLeft
             size="20"
             className="icon-back"
             onClick={() => setIsOpen(false)}
           />
-        ) : (
+        </>
+      ) : (
+        <div className="div-ranking">
+          <div className="div-notice-header"></div>
           <NavLink to="/manage" className="link-header">
             <HiOutlineArrowLeft size="20" className="icon-back" />
           </NavLink>
-        )}
-
-        <div className="div-month">회원 관리</div>
-        <div className="div-member-section-01">
-          <div className="div-member-updown-section">
-            <div className="div-member-updown">
-              <HiChevronUp
-                className={isColor == 0 && "icon-color"}
-                onClick={() => {
-                  setIsColor(0);
-                  getMember("name", "asc");
-                }}
-              />
-              <HiChevronDown
-                className={isColor == 1 && "icon-color"}
-                onClick={() => {
-                  setIsColor(1);
-                  getMember("name", "desc");
-                }}
-              />
+          <div className="div-month">회원 관리</div>
+          <div className="div-member-section-01">
+            <div className="div-member-updown-section">
+              <div className="div-member-updown">
+                <HiChevronUp
+                  className={isColor == 0 && "icon-color"}
+                  onClick={() => {
+                    setIsColor(0);
+                    getMember("name", "asc");
+                  }}
+                />
+                <HiChevronDown
+                  className={isColor == 1 && "icon-color"}
+                  onClick={() => {
+                    setIsColor(1);
+                    getMember("name", "desc");
+                  }}
+                />
+              </div>
+              <div className="div-member-updown">
+                <HiChevronUp
+                  className={isColor == 2 && "icon-color"}
+                  onClick={() => {
+                    setIsColor(2);
+                    getMember("join_date", "asc");
+                  }}
+                />
+                <HiChevronDown
+                  className={isColor == 3 && "icon-color"}
+                  onClick={() => {
+                    setIsColor(3);
+                    getMember("join_date", "desc");
+                  }}
+                />
+              </div>
             </div>
-            <div className="div-member-updown">
-              <HiChevronUp
-                className={isColor == 2 && "icon-color"}
-                onClick={() => {
-                  setIsColor(2);
-                  getMember("join_date", "asc");
-                }}
-              />
-              <HiChevronDown
-                className={isColor == 3 && "icon-color"}
-                onClick={() => {
-                  setIsColor(3);
-                  getMember("join_date", "desc");
-                }}
-              />
-            </div>
+            {showMember}
           </div>
-          {showMember}
         </div>
-      </div>
+      )}
     </>
   );
 }
