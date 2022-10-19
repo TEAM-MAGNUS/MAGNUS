24ac628dabaac857affe6f91f9da6ac6ef0992c9
GGyong <84078044+Gyudol1231@users.noreply.github.com>
2022-10-19 18:00:46 +0900

    .
    .

--- src/components/LoginData.js
+++ src/components/LoginData.js
@@ -1,2 +1,3 @@
 export const REST_API_KEY = '08066204288ab5d47ffcf2d53a17bdad';
+// export const REDIRECT_URI = 'https://teammagnus.net/kakaologin';
 export const REDIRECT_URI = 'http://localhost:3000/kakaologin';
