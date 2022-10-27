6926fc6215d2d702fb7c0fba08c52c7584abdcf1
changjini32 <84197474+changjini32@users.noreply.github.com>
2022-10-27 09:05:04 +0900

    .

--- src/components/Warning.js
+++ src/components/Warning.js
@@ -3,6 +3,8 @@ import { HiRefresh, HiOutlineArrowLeft } from "react-icons/hi";
 import { NavLink } from "react-router-dom";
 
 function Warning() {
+  const td = new Date();
+
   const [user, setUser] = useState([{}]);
   const getWarning = () => {
     fetch("https://teammagnus.net/getWarning", {
@@ -36,19 +38,20 @@ function Warning() {
         </NavLink>
         <div className="div-month">경고자</div>
         <div className="div-warning-section-01">
-          {user.length == 0 ? (
+          {td.getDay() === 0 || td.getDay() === 5 || td.getDay() === 6 ? (
+            <>
+              <div className="div-warning-notday-01">
+                명단 업데이트 중입니다!
+              </div>
+              <div className="div-warning-notday-02">
+                (훈련일에는 경고자를 확인하실 수 없습니다.)
+              </div>
+            </>
+          ) : user.length == 0 ? (
             <HiRefresh onClick={() => window.location.reload()} size="25" />
           ) : (
             showWarning
           )}
-          {/* {td.getDay() === 0 || td.getDay() === 5 || td.getDay() === 6 ? (
-            <>명단 업데이트 중입니다.</>
-          ) : 
-          user.length == 0 ? (
-            <HiRefresh onClick={() => window.location.reload()} size="25" />
-          ) : (
-            showWarning
-          )} */}
         </div>
       </div>
     </div>
